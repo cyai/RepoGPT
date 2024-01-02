508a66e521e57e7641ac4666b770ec4cab8dfd94
cyai <83634399+cyai@users.noreply.github.com>
2024-01-02 12:14:54 +0530

    del: removed django framework to replace with flask (future plan)

--- backend/repogpt/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import RepoGPT
-
-
-class RepoGPTSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = RepoGPT
-        fields = ("id", "repo", "question", "response")
-
